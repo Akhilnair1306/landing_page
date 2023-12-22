f8f195831255362c6ba8d68a5f01dbedb9656940
Akhilnair1306 <111660827+Akhilnair1306@users.noreply.github.com>
2023-12-22 19:38:23 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,34 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import React, { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
-export default function Header () {
+export default function Header() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
+              {/* Campus Credentials */}
+              <div className="flex items-center">
+                <p className="text-white text-sm font-medium pr-4">
+                  Campus Credentials
+                </p>
+              </div>
+
+              {/* Mobile menu button */}
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
@@ -32,32 +39,19 @@ export default function Header () {
                   )}
                 </Disclosure.Button>
               </div>
+
+              {/* Main Logo */}
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src= "D:\React Project\landing\src\components\WhatsApp Image 2023-12-16 at 7.28.50 PM.jpeg"
+                    src="D:\landing page\landing\src\components\Header.js"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
               </div>
+
+              {/* Notification and Profile */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
@@ -94,7 +88,7 @@ export default function Header () {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className="block px-4 py-2 text-sm text-gray-700"
                           >
                             Your Profile
                           </a>
@@ -104,7 +98,7 @@ export default function Header () {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className="block px-4 py-2 text-sm text-gray-700"
                           >
                             Settings
                           </a>
@@ -114,7 +108,7 @@ export default function Header () {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className="block px-4 py-2 text-sm text-gray-700"
                           >
                             Sign out
                           </a>
@@ -127,26 +121,49 @@ export default function Header () {
             </div>
           </div>
 
+          {/* Mobile Menu */}
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+              <div className="flex justify-center">
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="bg-gray-900 text-white px-3 py-2 text-base font-medium rounded-md">
+                      Menu
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      {navigation.map((item) => (
+                        <Menu.Item key={item.name}>
+                          {({ active }) => (
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}
+                            >
+                              {item.name}
+                            </a>
+                          )}
+                        </Menu.Item>
+                      ))}
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
             </div>
           </Disclosure.Panel>
         </>
       )}
     </Disclosure>
-  )
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,106 +10,68 @@ export default function Home()
         <div>
          <Header/>
         <div className="myComponent">
-        <div className="carousel w-2/3 mx-auto relative overflow-hidden aspect-w-1 aspect-h-1">
-  <div id="slide1" className="carousel-item relative w-full h-full aspect-w-1 aspect-h-1">
-  <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full h-full object-cover" alt="Slide 1" />
+        <div class="carousel relative overflow-hidden aspect-w-1 aspect-h-1">
+  <div id="slide1" class="carousel-item relative aspect-w-1 aspect-h-1">
+    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full h-full object-cover" alt="Slide 1" />
     <div class="absolute inset-0 flex flex-col justify-end p-5">
-      <div class="bg-white bg-opacity-80 p-4 rounded-md">
-        <p class="text-2xl font-bold mb-4">Your Text Here</p>
-      </div>
       <div class="flex justify-between">
         <a href="#slide4" class="btn btn-circle">❮</a> 
         <a href="#slide2" class="btn btn-circle">❯</a>
       </div>
     </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-full h-full aspect-w-1 aspect-h-1">
-    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full h-full object-cover" alt="Slide 2" />
-    <div className="absolute inset-0 flex flex-col justify-end p-5">
-      <div className="bg-white bg-opacity-80 p-4 rounded-md">
-        <p className="text-2xl font-bold mb-4">Your Text Here</p>
-      </div>
-      <div className="flex justify-between">
-        <a href="#slide1" className="btn btn-circle">❮</a> 
-        <a href="#slide3" className="btn btn-circle">❯</a>
-      </div>
-    </div>
-  </div>
-  <div id="slide3" className="carousel-item relative w-full h-full aspect-w-1 aspect-h-1">
-    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full h-full object-cover" alt="Slide 3" />
-    <div className="absolute inset-0 flex flex-col justify-end p-5">
-      <div className="bg-white bg-opacity-80 p-4 rounded-md">
-        <p className="text-2xl font-bold mb-4">Your Text Here</p>
-      </div>
-      <div className="flex justify-between">
-        <a href="#slide2" className="btn btn-circle">❮</a> 
-        <a href="#slide4" className="btn btn-circle">❯</a>
-      </div>
-    </div>
-  </div>
-  <div id="slide4" className="carousel-item relative w-full h-full aspect-w-1 aspect-h-1">
-    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full h-full object-cover" alt="Slide 4" />
-    <div className="absolute inset-0 flex flex-col justify-end p-5">
-      <div className="bg-white bg-opacity-80 p-4 rounded-md">
-        <p className="text-2xl font-bold mb-4">Your Text Here</p>
-      </div>
-      <div className="flex justify-between">
-        <a href="#slide3" className="btn btn-circle">❮</a> 
-        <a href="#slide1" className="btn btn-circle">❯</a>
-      </div>
-    </div>
-  </div>
 </div>
+
 
 <div class= "latest">
     <h2>Latest</h2>
 </div>
 <div class="Card">
-<div className="flex justify-center items-center">
-  <div className="card w-80 h-96 bg-base-100 shadow-xl m-5 ml-20">
-    <figure className="px-50 pt-50">
-      <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-    </figure>
-    <div className="card-body items-center text-center">
-      <h2 className="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions"></div>
+  <div class="flex flex-col md:flex-row justify-center items-center">
+    <div class="card w-full md:w-80 h-96 bg-base-100 shadow-xl m-5 md:ml-20">
+      <figure class="px-50 pt-50">
+        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions"></div>
+      </div>
     </div>
-  </div>
 
-  <div className="card w-80 h-96 bg-base-100 shadow-xl m-5 ml-20">
-    <figure className="px-50 pt-50">
-      <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-    </figure>
-    <div className="card-body items-center text-center">
-      <h2 className="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions"></div>
+    <div class="card w-full md:w-80 h-96 bg-base-100 shadow-xl m-5 md:ml-20">
+      <figure class="px-50 pt-50">
+        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions"></div>
+      </div>
     </div>
-  </div>
 
-  <div className="card w-80 h-96 bg-base-100 shadow-xl m-5 ml-20">
-    <figure className="px-50 pt-50">
-      <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-    </figure>
-    <div className="card-body items-center text-center">
-      <h2 className="card-title">Decoding the Data Universe: A Voyage into the World of Data Science</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions"></div>
+    <div class="card w-full md:w-80 h-96 bg-base-100 shadow-xl m-5 md:ml-20">
+      <figure class="px-50 pt-50">
+        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Decoding the Data Universe: A Voyage into the World of Data Science</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions"></div>
+      </div>
     </div>
   </div>
 </div>
 
-
-
-</div>
-<button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-teal-400 rounded-full p-2 w-100">explore more</button>
+<button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-teal-400 rounded-full p-2 sm:w-auto md:w-auto lg:w-auto">
+  explore more
+</button>
 <div class= "Rectangle">
 <div class= "topics">
     <h2>Topics</h2>
 </div>
 <div className="flex">
-  <div className="card w-96 bg-base-100 shadow-xl image-full flex flex-col mr-4">
+<div className="card w-full md:w-1/2 lg:w-1/4 bg-base-100 shadow-xl image-full flex flex-col mb-4 md:mb-0 lg:mb-0 md:mr-4 lg:mr-4">
     <figure>
       <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
     </figure>
@@ -118,7 +80,7 @@ export default function Home()
       <p>If a dog chews shoes whose shoes does he choose?</p>
     </div>
   </div>
-  <div className="card w-96 bg-base-100 shadow-xl image-full flex flex-col mr-4">
+  <div className="card w-full md:w-1/2 lg:w-1/4 bg-base-100 shadow-xl image-full flex flex-col mb-4 md:mb-0 lg:mb-0 md:mr-4 lg:mr-4">
     <figure>
       <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
     </figure>
@@ -127,7 +89,7 @@ export default function Home()
       <p>If a dog chews shoes whose shoes does he choose?</p>
     </div>
   </div>
-  <div className="card w-96 bg-base-100 shadow-xl image-full flex flex-col mr-4">
+  <div className="card w-full md:w-1/2 lg:w-1/4 bg-base-100 shadow-xl image-full flex flex-col mb-4 md:mb-0 lg:mb-0 md:mr-4 lg:mr-4">
     <figure>
       <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
     </figure>
@@ -137,7 +99,7 @@ export default function Home()
     </div>
   </div>
 
-  <div className="card w-96 bg-base-100 shadow-xl image-full flex flex-col ml-4">
+  <div className="card w-full md:w-1/2 lg:w-1/4 bg-base-100 shadow-xl image-full flex flex-col mb-4 md:mb-0 lg:mb-0 md:mr-4 lg:mr-4">
     <figure>
       <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
     </figure>
@@ -147,6 +109,7 @@ export default function Home()
     </div>
   </div>
 </div>
+
 
 
 
